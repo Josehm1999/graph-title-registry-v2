3e2b40428e3efc23cd63e40439272c877c57ae0c
JoseH <josehm1999@gmail.com>
2023-02-23 00:18:26 -0500

    Fix/Feat: Changed entities to map objects instead of events and finished the flow for the handleEvent functions

--- src/title-registry-v-2.ts
+++ src/title-registry-v-2.ts
@@ -9,92 +9,86 @@ import {
 } from '../generated/titleRegistryV2/titleRegistryV2';
 import {
   PropertyBought,
-  PropertyChangedAvailability,
   PropertyListed,
-  PropertyRequestStatusChanged,
-  RegionalAdminCreated,
-  AvailableProperty,
-  TransactionCanceled
+  RegionalAdmin
 } from '../generated/schema';
 
 export function handlePropertyBought(event: PropertyBoughtEvent): void {
-  let propertyBought = PropertyBought.load(
+  let property_listed = PropertyListed.load(
+    getIdFromEventParams(event.params.surveyNumber, event.params.seller)
+  );
+
+  let property_bought = PropertyBought.load(
     getIdFromEventParams(event.params.surveyNumber, event.params.buyer)
   );
 
-  let availableProperty = AvailableProperty.load(
-    getIdFromEventParams(event.params.surveyNumber, event.params.buyer)
-  );
-
-  if (!propertyBought) {
-    propertyBought = new PropertyBought(
+  if (!property_bought) {
+    property_bought = new PropertyBought(
       getIdFromEventParams(event.params.surveyNumber, event.params.buyer)
     );
   }
 
-  propertyBought.buyer = event.params.buyer;
-  propertyBought.surveyNumber = event.params.surveyNumber;
-  availableProperty!.buyer = event.params.buyer;
-  propertyBought.save();
-  availableProperty!.save();
+  property_bought.seller = event.params.seller;
+  property_bought.buyer = event.params.buyer;
+  property_bought.surveyNumber = event.params.surveyNumber;
+  property_bought.marketValue = event.params.marketValue;
+  property_bought.updatedAt = event.block.timestamp;
+  property_bought.save();
+
+  property_listed!.seller = event.params.buyer;
+  property_listed!.ReqStatus = 0;
+  property_listed!.isAvailable = false;
+  property_listed!.updatedAt = event.block.timestamp;
+  property_listed!.save();
 }
 
 export function handlePropertyChangedAvailability(
   event: PropertyChangedAvailabilityEvent
-): void {}
-
-export function handlePropertyListed(event: PropertyListedEvent): void {
-  let propertyListed = PropertyListed.load(
+): void {
+  let property_listed = PropertyListed.load(
     getIdFromEventParams(event.params.surveyNumber, event.params.seller)
   );
 
-  if (!propertyListed) {
-    propertyListed = new PropertyListed(
+  property_listed!.isAvailable = event.params.isAvailable;
+  property_listed!.save();
+}
+
+export function handlePropertyListed(event: PropertyListedEvent): void {
+  let property_listed = PropertyListed.load(
+    getIdFromEventParams(event.params.surveyNumber, event.params.seller)
+  );
+
+  if (!property_listed) {
+    property_listed = new PropertyListed(
       getIdFromEventParams(event.params.surveyNumber, event.params.seller)
     );
   }
 
-  propertyListed.seller = event.params.seller;
-  propertyListed.surveyNumber = event.params.surveyNumber;
-  propertyListed.state = event.params.state;
-  propertyListed.district = event.params.district;
-  propertyListed.neighborhood = event.params.neighborhood;
-  propertyListed.marketValue = event.params.marketValue;
-  propertyListed.isAvailable = event.params.isAvailable;
-  propertyListed.save();
+  property_listed.seller = event.params.seller;
+  property_listed.surveyNumber = event.params.surveyNumber;
+  property_listed.state = event.params.state;
+  property_listed.district = event.params.district;
+  property_listed.neighborhood = event.params.neighborhood;
+  property_listed.marketValue = event.params.marketValue;
+  property_listed.isAvailable = event.params.isAvailable;
+  property_listed.save();
 }
 
 export function handlePropertyStatusChanged(
   event: PropertyRequestStatusChangedEvent
 ): void {
-  let property_status_change = PropertyRequestStatusChanged.load(
-    getIdFromEventParams(event.params.surveyNumber, event.params.seller)
-  );
-
-  if (!property_status_change) {
-    property_status_change = new PropertyRequestStatusChanged(
-      getIdFromEventParams(event.params.surveyNumber, event.params.seller)
-    );
-  }
-
   let propertyListed = PropertyListed.load(
     getIdFromEventParams(event.params.surveyNumber, event.params.seller)
   );
 
-  let propertyAvailable = AvailableProperty.load(
-    getIdFromEventParams(event.params.surveyNumber, event.params.seller)
-  );
-
   propertyListed!.ReqStatus = event.params.param2;
-  if (propertyAvailable) {
-    propertyAvailable!.ReqStatus = event.params.param2;
-  }
+  propertyListed!.save();
 }
 
 export function handleRegionalAdminCreated(
   event: RegionalAdminCreatedEvent
 ): void {
-  let new_regional_admin = RegionalAdminCreated.load(
+  let new_regional_admin = RegionalAdmin.load(
     getIdFromEventParamsWithString(
       event.params.district,
       event.params.regionalAdmin
@@ -102,7 +96,7 @@ export function handleRegionalAdminCreated(
   );
 
   if (!new_regional_admin) {
-    new_regional_admin = new RegionalAdminCreated(
+    new_regional_admin = new RegionalAdmin(
       getIdFromEventParamsWithString(
         event.params.district,
         event.params.regionalAdmin
@@ -118,30 +112,11 @@ export function handleRegionalAdminCreated(
 export function handleTransactionCanceled(
   event: TransactionCanceledEvent
 ): void {
-  let transactionCanceled = TransactionCanceled.load(
+  let property_listed = PropertyListed.load(
     getIdFromEventParams(event.params.surveyNumber, event.params.seller)
   );
-
-  let availableProperty = AvailableProperty.load(
-    getIdFromEventParams(event.params.surveyNumber, event.params.seller)
-  );
-
-  if (!transactionCanceled) {
-    transactionCanceled = new TransactionCanceled(
-      getIdFromEventParams(event.params.surveyNumber, event.params.seller)
-    );
-  }
-
-  transactionCanceled.seller = event.params.seller;
-  transactionCanceled.surveyNumber = event.params.surveyNumber;
-  availableProperty!.ReqStatus = 0; //DEFAULT STATUS
-}
-
-function getIdFromEventParamsWithNumbers(
-  surveyNumber: BigInt,
-  status: Number
-): string {
-  return surveyNumber.toHexString() + status.toString();
+  property_listed!.ReqStatus = 0;
+  property_listed!.save();
 }
 
 function getIdFromEventParams(surveyNumber: BigInt, buyer: Address): string {
